53218b09f1dc205911a65a26cc385be0f42b66b4
michelh-fer <mmh51669@fer.hr>
2021-01-16 21:40:04 +0100

    dodan dio 4. labosa

--- or-lab3/routes/index.routes.js
+++ or-lab3/routes/index.routes.js
@@ -5,6 +5,13 @@ var videogame = require('../models/VideogameModel');
 var proizvodac = require('../models/ProizvodacModel');
 var lik = require('../models/LikoviModel');
 var izdavac = require('../models/IzdavaciModel');
+const axios = require('axios')
+const fs = require('fs');
+var path = require('path');
+
+//za cache
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache( { stdTTL: 100, checkperiod: 120 } );
 
 router.get('/videoigre', async function(req, res, next) {
     
@@ -66,6 +73,9 @@ router.get('/videoigre/:videogameID', async function(req, res, next) {
     try {
         let game = await videogame.fetchByVideogameId(req.params.videogameID); 
 
+        //dodaj link na sliku
+        game.slika = req.protocol + '://' + req.get('host') + req.originalUrl + '/picture';
+
         if (game != null) {
 
             let videogameSchema = {
@@ -108,6 +118,90 @@ router.get('/videoigre/:videogameID', async function(req, res, next) {
             }
         );
     }   
+
+});
+
+router.get('/videoigre/:videogameID/picture', async function(req, res, next) {
+
+    const imagePath = path.join(__dirname, `../public/images/${req.params.videogameID}.png`);
+
+    //provjerava je li isteklo vrijeme trajanja, ako je onda value == undefined
+    var value = myCache.get( req.params.videogameID );
+
+    //ako slika ne postoji onda je dohvati ili ako je isteklo vrijeme trajanja
+    if (!fs.existsSync(imagePath) || value == undefined) {
+
+        var game = await videogame.fetchByVideogameId(req.params.videogameID); 
+        
+        if (game == null) {
+            res.setHeader('Content-Type', 'application/json');
+
+            res.status(404).json(
+                {
+                    "status": "Not Found",
+                    "message": "Videoigra sa danim ID-om nije pronađena",
+                    "reponse": null
+                }
+            );
+            return;
+        }
+
+        //spremi kljuc u cache -> trenutno postavljeno vrijeme od 20 sek
+        var success = myCache.set( game.videoigraid, game.nazivvideoigra, 20 ); //key, val, vrijeme trajanja (u sek)
+
+        //ako je uspjesno cacheirano onda ispisi
+        if (success) {
+            var today = new Date();
+            var date = today.getDate() + '.' + (today.getMonth() + 1) + '.' + today.getFullYear() + '.';
+            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            var dateTime = date + ' ' + time;
+            
+            console.log(dateTime + " keširano " + game.nazivvideoigra + " (ID: " + game.videoigraid + ")");
+        }
+
+        let wikHandle = game.wikipedia_stranica;
+
+        const url =  `https://en.wikipedia.org/api/rest_v1/page/summary/${wikHandle}`;
+        
+        const getData = async (url) => {
+            try {
+                const response = await axios.get(url);
+                const data = response.data;
+                //console.log(data);
+                return data;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        
+        const download_image = async (url, image_path) =>
+            axios({
+                url,
+                responseType: 'stream',
+            }).then(
+                response =>
+                new Promise((resolve, reject) => {
+                    response.data
+                    .pipe(fs.createWriteStream(image_path))
+                    .on('finish', () => resolve())
+                    .on('error', e => reject(e));
+                }),
+            );
+
+
+        var data = await getData(url);
+
+        var image = data.thumbnail.source;
+
+        await download_image(image, `./public/images/${req.params.videogameID}.png`);
+
+    }
+
+    //postavljanje headera
+    res.setHeader('content-type', 'image/png');
+    res.setHeader('content-disposition','inline');
+
+    res.sendFile(imagePath);
 
 });
 
